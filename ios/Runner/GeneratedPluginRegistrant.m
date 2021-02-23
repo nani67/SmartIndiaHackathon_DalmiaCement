be428b8aa4652240a382ca792fd03e8dccd481fb
P Nani <44634884+nani67@users.noreply.github.com>
2021-02-23 20:38:56 +0530

    CI/CD using Github Actions

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,15 +3,60 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<cloud_firestore/CloudFirestorePlugin.h>)
 #import <cloud_firestore/CloudFirestorePlugin.h>
+#else
+@import cloud_firestore;
+#endif
+
+#if __has_include(<firebase_auth/FirebaseAuthPlugin.h>)
 #import <firebase_auth/FirebaseAuthPlugin.h>
+#else
+@import firebase_auth;
+#endif
+
+#if __has_include(<firebase_core/FirebaseCorePlugin.h>)
 #import <firebase_core/FirebaseCorePlugin.h>
+#else
+@import firebase_core;
+#endif
+
+#if __has_include(<flutter_search_panel/FlutterSearchPanelPlugin.h>)
 #import <flutter_search_panel/FlutterSearchPanelPlugin.h>
+#else
+@import flutter_search_panel;
+#endif
+
+#if __has_include(<google_sign_in/GoogleSignInPlugin.h>)
 #import <google_sign_in/GoogleSignInPlugin.h>
+#else
+@import google_sign_in;
+#endif
+
+#if __has_include(<location/LocationPlugin.h>)
 #import <location/LocationPlugin.h>
+#else
+@import location;
+#endif
+
+#if __has_include(<simple_permissions/SimplePermissionsPlugin.h>)
 #import <simple_permissions/SimplePermissionsPlugin.h>
+#else
+@import simple_permissions;
+#endif
+
+#if __has_include(<speech_recognition/SpeechRecognitionPlugin.h>)
 #import <speech_recognition/SpeechRecognitionPlugin.h>
+#else
+@import speech_recognition;
+#endif
+
+#if __has_include(<tts/TtsPlugin.h>)
 #import <tts/TtsPlugin.h>
+#else
+@import tts;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
